9d3bde5ecacb828fdd138a88fb2e5c8f6ea52db3
Mehran <alonephoenix90@gmail.com>
2024-09-17 11:51:24 +0330

    -vercel cli installed and link to project -prisma database ORM installed and model schema changes a little bit but not compeleted yet

--- app/(homepage)/signin/page.tsx
+++ app/(homepage)/signin/page.tsx
@@ -1,19 +1,46 @@
+'use client'
+import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
 import { FaRegUser } from "react-icons/fa";
 import { FaKey } from "react-icons/fa";
 
 export default function Signin() {
+    const router = useRouter();
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get('email');
+        const pass = formData.get('password');
+
+        const res = await fetch('', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email, pass
+            })
+        })
+
+        console.log(res);
+        if(res.ok){
+            //router.push('/dashboard');
+        }else{
+            //handle error
+        }
+    }
     return <main className="flex justify-center">
         <div className="card bg-base-100 w-full md:w-96 shadow-xl">
             <div className="card-body ">
                 <div className="card-title justify-center mb-6">Login</div>
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <label className="input input-bordered flex items-center gap-2 mb-4">
                         <FaRegUser />
-                        <input type="text" className="grow" placeholder="Email" />
+                        <input name="email" type="text" className="grow" placeholder="Email" />
                     </label>
                     <label className="input input-bordered flex items-center gap-2 mb-4">
                         <FaKey />
-                        <input type="password" className="grow" value="password" />
+                        <input name="password" type="password" className="grow" value="password" />
                     </label>
                     <button className="btn btn-secondary"> Login </button>
                 </form>
